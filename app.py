2a24edcb47ae90fd7fe563dcd2f18d43680d8cfc
Ruslan Rudiuk <rus.barik@gmail.com>
2024-10-15 17:38:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import os
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    # Logic to generate or retrieve your data
-    data = {
+def home():
+    return jsonify({
         "version": "1.0",
         "message": "Hello from the API!"
-    }
-    return jsonify(data)
+    })
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
